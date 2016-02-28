5f9836be9a296f2fa426463d3bdc1805663e7a7e
Alex Kras <akras14@gmail.com>
2016-02-27 21:27:22 -0800

    Working with video expand modal

--- source.js
+++ source.js
@@ -16,14 +16,18 @@ angular.module('myapp', [])
         }
       })
       .then(function(res) {
-        console.log(res);
+        if(!res.data || !res.data.gfycats){
+          console.error("No videos returned");
+          $scope.videos = [];
+          return;
+        }
         $scope.videos = res.data.gfycats.map(function(video){
           video.hover = false;
           return video;
         });
       })
       .catch(function(err) {
-        console.log(err);
+        console.error(err);
       });
       $scope.hoverOn = function(video){
         video.hover = true;
@@ -31,7 +35,13 @@ angular.module('myapp', [])
       $scope.hoverOff = function(video){
         video.hover = false;
       };
-
+      $scope.showModal = function(video){
+        video.hover = false;
+        $scope.modalVideo = video;
+      };
+      $scope.hideModal = function(){
+        $scope.modalVideo = null;
+      };
     }
     return {
       scope: {},
@@ -42,6 +52,7 @@ angular.module('myapp', [])
   })
   .directive('videoContainer', function(){
     return {
+      restrict: 'E',
       scope: {
         previewUrl: '=',
         mp4Url: '=',
@@ -49,5 +60,20 @@ angular.module('myapp', [])
       },
       transclude: true,
       templateUrl: 'videoContainer.html'
+    };
+  })
+  .directive('modal', function(){
+    function controller($scope){
+      $scope.hide = $scope.hideModal();
+    }
+    return {
+      restrict: 'E',
+      scope: {
+        hideModal: '&',
+        source: '='
+      },
+      transclude: true,
+      controller: controller,
+      templateUrl: 'modal.html'
     };
   });
